2761baddbe03424269ff7caafdf2255f42bd60c5
John Johnson <dampboxdev@gmail.com>
2013-09-30 20:20:11 -0700

    added momentjs for timestamp

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask.ext.mail import Mail
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from momentjs import momentjs
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -35,4 +37,5 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.info('microblog startup')
 
+app.jinja_env.globals['momentjs'] = momentjs
 from app import views, models
