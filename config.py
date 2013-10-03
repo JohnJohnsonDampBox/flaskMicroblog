519b67c01cd7f4388fc0bc76be0298946e087b18
John Johnson <dampboxdev@gmail.com>
2013-10-02 22:54:42 -0700

    Updated config.py

--- config.py
+++ config.py
@@ -5,8 +5,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'doodoo'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False'
+else:
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+
+WHOOSH_ENABLED = os.environ.get('HEROKU') is None
+
 
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
@@ -26,10 +34,6 @@ ADMINS = ['dampboxdev@gmail.com']
 
 POSTS_PER_PAGE = 3
 
-WHOOSH_BASE = os.path.join(basedir, 'search.db')
-
-WHOOSH_ENABLED = os.environ.get('HEROKU') is None
-
 MAX_SEARCH_RESULTS = 50
 
 LANGUAGES = {
@@ -37,5 +41,4 @@ LANGUAGES = {
 	'es': 'Espanol'
 }
 
-SQLALCHEMY_RECORD_QUERIES = True
 DATABASE_QUERY_TIMEOUT = 0.5
